9a0630dfb6fcd19adfaf1a0ea2bc42d227e7dad2
alykes <anthony.gouskos@gmail.com>
2021-01-24 10:37:44 +1100

    Removed intentional failure

--- main-test.py
+++ main-test.py
@@ -4,7 +4,6 @@ from main import Add
 def TestAdd():
         assert Add(2,3) == 5
         assert Add(5,5) == 10
-        assert Add(8,8) == 100
         print("Add funtion is working correctly")
 
 if __name__ == '__main__':
